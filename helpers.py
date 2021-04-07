e4347b920b9fa48cbc2eadc0887c6a7c4ac83c4f
Niels Asp Fuglsang <nielsaspfuglsang@gmail.com>
2021-04-07 15:00:06 +0200

    Autoformat.

--- helpers.py
+++ helpers.py
@@ -1,13 +1,16 @@
 import numpy as np
 
+
 def read_mnist():
     """Read file and labels."""
     f = open('mnist/train-images-idx3-ubyte', 'r')
-    a = np.fromfile(f, dtype='>i4', count=4) # data type is signed integer big-endian
+    # data type is signed integer big-endian
+    a = np.fromfile(f, dtype='>i4', count=4)
     images = np.fromfile(f, dtype=np.uint8)
 
     f = open('mnist/train-labels-idx1-ubyte', 'r')
-    t = np.fromfile(f, count = 2, dtype='>i4') # data type is signed integer big-endian
+    # data type is signed integer big-endian
+    t = np.fromfile(f, count=2, dtype='>i4')
     labels = np.fromfile(f, dtype=np.uint8)
 
     ims = images.reshape(a[1:])
@@ -17,20 +20,22 @@ def read_mnist():
 
 def ReLU(x, derivative=False):
     if derivative:
-        return (x>0).astype(int)
+        return (x > 0).astype(int)
     else:
         return np.maximum(x, 0)
 
+
 def softmax(x, derivative=False):
     if derivative:
-        return np.diagflat(x) 
+        return np.diagflat(x)
     else:
         return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
 
+
 def cross_entropy_loss(t, y, derivative=False):
-    if np.shape(t)!=np.shape(y):
+    if np.shape(t) != np.shape(y):
         print("t and y have different shapes")
-    if derivative: # Return the derivative of the function
+    if derivative:  # Return the derivative of the function
         return y-t
     else:
         return t * np.log(y)
