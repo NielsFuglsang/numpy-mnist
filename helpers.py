5b16f002bfdbcb07f54a2da305991dc3dc9afe1e
Niels Asp Fuglsang <nielsaspfuglsang@gmail.com>
2021-04-07 14:52:07 +0200

    Changed project structure and moved read function

--- helpers.py
+++ helpers.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+def read_mnist():
+    """Read file and labels."""
+    f = open('mnist/t10k-images-idx3-ubyte', 'r')
+    a = np.fromfile(f, dtype='>i4', count=4) # data type is signed integer big-endian
+    images = np.fromfile(f, dtype=np.uint8)
+
+    f = open('mnist/t10k-labels-idx1-ubyte', 'r')
+    t = np.fromfile(f, count = 2, dtype='>i4') # data type is signed integer big-endian
+    labels = np.fromfile(f, dtype=np.uint8)
+
+    ims = images.reshape(a[1:])
+
+    return ims, labels, a, t
+
+
 def ReLU(x, derivative=False):
     if derivative:
         return (x>0).astype(int)
