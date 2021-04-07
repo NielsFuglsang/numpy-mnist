00dc5fbd6fa2acb35287e845ff86f388bb2f2dcc
madsemildahlgaard <madsemildh@gmail.com>
2021-04-07 15:48:38 +0200

    Add functions for iterating through minibatches

--- helpers.py
+++ helpers.py
@@ -3,13 +3,13 @@ import numpy as np
 
 def read_mnist():
     """Read file and labels."""
-    f = open('mnist/train-images-idx3-ubyte', 'r')
+    f = open('mnist/t10k-images-idx3-ubyte', 'r')
     # data type is signed integer big-endian
     a = np.fromfile(f, dtype='>i4', count=4)
     images = np.fromfile(f, dtype=np.uint8)
     f.close()
 
-    f = open('mnist/train-labels-idx1-ubyte', 'r')
+    f = open('mnist/t10k-labels-idx1-ubyte', 'r')
     # data type is signed integer big-endian
     t = np.fromfile(f, count=2, dtype='>i4')
     labels = np.fromfile(f, dtype=np.uint8)
@@ -35,7 +35,6 @@ def stable_softmax(X):
     return softmax
 
 
-
 def cross_entropy_loss(t, y, derivative=False):
     if np.shape(t) != np.shape(y):
         print("t and y have different shapes")
@@ -43,3 +42,12 @@ def cross_entropy_loss(t, y, derivative=False):
         return y-t
     else:
         return t * np.log(y)
+
+
+def iterate_minibatches(inputs, targets, batchsize):
+    """Generator for getting minibatches."""
+    indices = np.arange(inputs.shape[0])
+    np.random.shuffle(indices)
+    for start_idx in range(0, inputs.shape[0] - batchsize + 1, batchsize):
+        excerpt = indices[start_idx:start_idx + batchsize]
+        yield inputs[excerpt], targets[excerpt]
