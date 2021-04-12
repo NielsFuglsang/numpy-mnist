d15e2e615c19a0bde105d1deebab3215b13d6200
madsemildahlgaard <madsemildh@gmail.com>
2021-04-12 13:00:44 +0200

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from nn import NeuralNetwork
 from helpers import read_mnist, format_data, iterate_minibatches, split_data, accuracy, dump_nn
+from augmentation import load_augmented
 
 def train(nn, X_train, y_train, X_val, y_val, epochs, batch_size):
     """Training loop."""
@@ -25,20 +26,25 @@ def train(nn, X_train, y_train, X_val, y_val, epochs, batch_size):
         yhat_val, _, _ = nn.forward(X_val)
         acc_val = accuracy(yhat_val, y_val)
         accuracy_val.append(acc_val)
+        print(f"Epoch {i}, Val acc: {acc_val}", flush=True)
 
     return accuracy_train, accuracy_val
 
 
 # Read and format data.
 ims, labels, a, t = read_mnist()
+ims_aug, labels_aug = load_augmented()
 X, y = format_data(ims, labels)
+X_aug, y_aug = format_data(ims_aug, labels_aug)
 X_train, y_train, X_val, y_val = split_data(X, y, percentage=80)
+X_train = np.append(X_train, X_aug, axis=0)
+y_train = np.append(y_train, y_aug, axis=0)
 
 # Create nn.
-nn = NeuralNetwork(layers=[784, 400, 200, 10], momentum=0.9)
+nn = NeuralNetwork(layers=[784, 800, 10], momentum=0.9)
 
 # Train.
-epochs = 1
+epochs = 300
 batch_size = 200
 accuracy_train, accuracy_val = train(nn, X_train, y_train, X_val, y_val, epochs, batch_size)
 
@@ -46,3 +52,4 @@ yhat_val, _, _ = nn.forward(X_val)
 acc = accuracy(yhat_val, y_val)
 
 dump_nn(nn, f'models/{acc:.3f}.pkl')
+
